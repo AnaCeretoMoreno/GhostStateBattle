084610fc84dc5d7906316528d5d690e27b3ad4c7
NokkelDag <114680012+NokkelDag@users.noreply.github.com>
2025-05-24 19:57:25 +0200

    Delete Assets directory

--- Assets/Scripts/OurGame/autodestroyParticles.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class autodestroyParticles : MonoBehaviour
-{
-    void Start()
-    {
-        Destroy(gameObject, GetComponent<ParticleSystem>().main.duration + 0.5f);
-    }
-}
